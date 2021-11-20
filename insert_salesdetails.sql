4b834e0212c2eb126e2cea3cd87bd05b78d21af1
suriyaprakashj <93181603+suriyaprakashj@users.noreply.github.com>
2021-11-20 12:07:17 -0800

    Create insert_salesdetails.sql

---
+++ insert_salesdetails.sql
@@ -0,0 +1,10 @@
+insert into salesdetails values ('SS02','BB02',4,2);
+insert into salesdetails values ('SS03','BB03',2,1);
+insert into salesdetails values ('SS04','BB04',5,2);
+insert into salesdetails values ('SS05','BB05',5 , 1);
+insert into salesdetails values ('SS07','BB07',7 , 1);
+insert into salesdetails values ('SS08','BB08',8 , 1);
+insert into salesdetails values ('SS09','BB09',7 , 1);
+insert into salesdetails values ('SS10','BB10',9 , 1);
+insert into salesdetails values ('SS01','BB01',4 , 1);
+insert into salesdetails values ('SS06','BB06',8 , 1);
