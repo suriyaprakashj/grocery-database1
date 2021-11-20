9baaa04305ed3c4110abbf9c774c5af0cc6334d8
suriyaprakashj <93181603+suriyaprakashj@users.noreply.github.com>
2021-11-20 12:11:25 -0800

    Create insert_sales.sql

---
+++ insert_sales.sql
@@ -0,0 +1,9 @@
+insert into sales values ('SS05','AA04','2021-11-11');
+ insert into sales values ('SS06','AA05','2021-11-11');
+ insert into sales values ('SS07','AA05','2021-11-11');
+ insert into sales values ('SS08','AA06','2021-11-11');
+ insert into sales values ('SS10','AA08','2021-11-11');
+ insert into sales values ('SS09','AA07','2021-11-11');
+ insert into sales values ('SS03','AA02','2021-11-11');
+ 
+ 
