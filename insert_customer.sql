ef7b993b424af1995424f0ad934b349b6b45962f
suriyaprakashj <93181603+suriyaprakashj@users.noreply.github.com>
2021-11-20 11:58:19 -0800

    Create insert_customer.sql

---
+++ insert_customer.sql
@@ -0,0 +1,6 @@
+insert into  Customeer (CustomerID, CustomerFirstName , CustomerLastName  ,Address , City , State , ZipCode , phonenumber) values ("AA02","sunny","jain", "100Avenue","chicago","chicago",98002,206705709);
+insert into  Customeer (CustomerID, CustomerFirstName , CustomerLastName  ,Address , City , State , ZipCode , phonenumber) values ("AA03","jimmy","jim", "101Avenue","naperville","chicago",98002,206705700);
+insert into  Customeer (CustomerID, CustomerFirstName , CustomerLastName  ,Address , City , State , ZipCode , phonenumber) values ("AA01","surya","prakash", "100Avenue","chicago","chicago",98002,206705708);
+insert into  Customeer (CustomerID, CustomerFirstName , CustomerLastName  ,Address , City , State , ZipCode , phonenumber) values ("AA07","king","jimmy", "102Avenue","romeoville","chicago",98004,206705704);
+insert into  Customeer (CustomerID, CustomerFirstName , CustomerLastName  ,Address , City , State , ZipCode , phonenumber) values ("AA09","madi","julie", "12Avenue","lilleville","chicago",98004,206705709);
+insert into  Customeer (CustomerID, CustomerFirstName , CustomerLastName  ,Address , City , State , ZipCode , phonenumber) values ("BB123","ram","jan", "12Avenue","lilleville","chicago",98004,206705712);
